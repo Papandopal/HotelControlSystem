820ed98634182704258327674c7cc4163c92a713
Papandopal <obezan8@gmail.com>
2026-07-06 15:06:53 +0300

    feat:add funcs and DTOs in Adapters

--- Adapters/Controllers/Console/BookingController.cs
+++ Adapters/Controllers/Console/BookingController.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Adapters.DTOs.BookingDTOs;
+using AutoMapper;
+using DoMain.Entities;
+using UseCase.DTOs.BookingDTOs;
+using UseCase.Services.BookingService;
 
 namespace Adapters.Controllers.Console
 {
-    public class BookingController
+    public class BookingController(IBookingService bookingService, IMapper mapper)
     {
+        public void Create(CreateBookingDTO createBookingDTO)
+        {
+            bookingService.Create(mapper.Map<CreateBookingUseCaseDTO>(createBookingDTO));
+        }
+        public void Cancel(int id)
+        {
+            bookingService.Cancel(id);
+        }
+
+        public List<BookingInfoForUserDTO> GetAllForUser(int userId)
+        {
+            return mapper.Map<List<BookingInfoForUserDTO>>(bookingService.GetAllForUser(userId));
+        }
+
+        public List<BookingInfoForAdminsDTO> GetAll()
+        {
+            return mapper.Map<List<BookingInfoForAdminsDTO>>(bookingService.GetAll());
+        }
+
+        public List<BookingInfoForAdminsDTO> GetAllByUser(int userId)
+        {
+            return mapper.Map<List<BookingInfoForAdminsDTO>>(bookingService.GetAllByUser(userId));
+        }
+
+        public List<BookingInfoForAdminsDTO> GetAllByRoom(int roomId)
+        {
+            return mapper.Map<List<BookingInfoForAdminsDTO>>(bookingService.GetAllByRoom(roomId));
+        }
+
     }
 }
---
+++ Adapters/DTOs/BookingDTOs/BookingInfoForAdminsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoMain.Enums;
+
+namespace Adapters.DTOs.BookingDTOs
+{
+    public class BookingInfoForAdminsDTO
+    {
+        public int Id { get; init; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public BookingStatus Status { get; set; }
+    }
+}
